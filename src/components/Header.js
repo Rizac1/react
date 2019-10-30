6da1d8f07d4fd006a43af9e88336561ab485d4a4
Rizac1 <55118545+Rizac1@users.noreply.github.com>
2019-10-29 20:13:26 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+// import React from "react";
+
+// const Header = props => {
+//   return (
+//     <div>
+//       <div className="logo-container">
+//         <img src={Logo} />
+//       </div>
+//       <div className="title">Todo List</div>
+//     </div>
+//   );
+// };
+
+// export default Header;
